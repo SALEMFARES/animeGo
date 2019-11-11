df0e21b431fbd348d18de7fa933e30810809c19e
SALEMFARES <43521133+SALEMFARES@users.noreply.github.com>
2019-11-11 01:45:04 +0100

    second commit

--- backend/src/main/java/entities/AmineCharacter.java
+++ backend/src/main/java/entities/AmineCharacter.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class AmineCharacter {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+
+public class AmineCharacter implements Serializable {
+     @Id
+     @GeneratedValue
+
+	 private Long idAnime;
+	 private String animeName;
+	 private String category;
+	 private String strength;
+	 private byte[] photo;
+	 private boolean shared;
+	 
+	 @ManyToOne
+	 @JoinColumn(name = "idUser")
+	 private User user;
+	 
 }
--- backend/src/main/java/entities/User.java
+++ backend/src/main/java/entities/User.java
@@ -1,5 +1,38 @@
 package entities;
+import java.io.Serializable;
+import java.util.List;
 
-public @interface User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements Serializable {
+    @Id 
+    @GeneratedValue
+    
+	private Long idUser;
+	private String firstName;
+	private String lastName;
+	private String mail;
+	private String password;
+	@Lob
+	private byte[] photo;
+	
+	@OneToMany(mappedBy = "user")
+	private List<AmineCharacter> characters;
+	
 }
